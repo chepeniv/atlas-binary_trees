7c9e39332b16c77af436018947fee2e790fb624f
chepeniv <jose.olmos@atlasschool.com>
2024-08-09 11:48:03 -0500

    [passing] implemented mirroring logic

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - creates a new leaf node unto the left of the
+ * binary_tree_insert_right - creates a new leaf node unto the right of the
  * given node
  * @parent: parent node to pass on the new child node to
  * @value: value to initialize node with
  *
  * Return: pointer to the newly created node or NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new, *hold;
 
@@ -21,14 +21,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new->n = value;
 	new->parent = parent;
-	new->right = NULL;
+	new->left = NULL;
 
 	/* the new node pushes the old node down, it does not replace it */
-	hold = parent->left;
+	hold = parent->right;
 	if (hold != NULL)
 		hold->parent = new;
-	new->left = hold;
-	parent->left = new;
+	new->right = hold;
+	parent->right = new;
 
 	return (new);
 }
