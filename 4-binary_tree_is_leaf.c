01695794a44c129d35f735c0a8b667df7f3fcb5d
chepeniv <jose.olmos@atlasschool.com>
2024-08-09 18:32:18 -0500

    [untested] had logic backwards; added doc

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,16 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf -
- * @node:
+ * binary_tree_is_leaf - checks whether given node is a leaf
+ * @node: node to analyze
  *
- * Return: void
+ * Return: 1 for leaf, 0 otherwise
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if
-	(node == NULL ||
-	(node->left != NULL && node->right != NULL))
+	(
+	node == NULL ||
+	node->left != NULL ||
+	node->right != NULL
+	)
 		return (0);
 
 	return (1);
