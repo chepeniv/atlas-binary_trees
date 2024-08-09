3accdcd238c38d275144d6c15f198c3199aaa768
chepeniv <jose.olmos@atlasschool.com>
2024-08-09 05:28:31 -0500

    [tested] completed task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - creates a new leaf node unto the left of the given node
+ * @parent: parent node to pass on the new child node to
+ * @value: value to initialize node with
+ *
+ * Return: pointer to the newly created node or NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL || parent == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+
+	if (parent->left != NULL)
+		new->left = parent->left;
+	else
+		new->left = NULL;
+
+	parent->left = new;
+
+	return (new);
+}
