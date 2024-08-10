771b781dccd0ca346dc4dde037d9ecadab03ec6d
chepeniv <jose.olmos@atlasschool.com>
2024-08-10 12:05:15 -0500

    [untested] added NULL-case check

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a tree
- * @node: point from which to count from
+ * binary_tree_depth - measures how deep a node in a tree is
+ * @node: node to measure
  *
- * Return: height measured
+ * Return: depth measured
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t count;
 
-	if (node->parent == NULL)
+	if (node->parent == NULL || node == NULL)
 		return (0);
 
 	count = 1;
