b32734a12b6eca673c8288de7a4841cf5fbf15d9
chepeniv <jose.olmos@atlasschool.com>
2024-08-10 15:23:44 -0500

    [passing] added binary_tree_hight() def to fil

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,8 +17,33 @@ int binary_tree_balance(const binary_tree_t *node)
 	if (node->right == NULL)
 		count--;
 
-	count += binary_tree_nodes(node->left);
-	count -= binary_tree_nodes(node->right);
+	count += binary_tree_height(node->left);
+	count -= binary_tree_height(node->right);
+
+	return (count);
+}
+
+/**
+ * binary_tree_height - measures the height of a tree
+ * @node: point from which to count from
+ *
+ * Return: height measured
+ */
+size_t binary_tree_height(const binary_tree_t *node)
+{
+	size_t count = 0, left_count, right_count;
+
+	if (node == NULL || (node->left == NULL && node->right == NULL))
+		return (0);
+
+	count = 1;
+	left_count = binary_tree_height(node->left);
+	right_count = binary_tree_height(node->right);
+
+	if (left_count >= right_count)
+		count += left_count;
+	else
+		count += right_count;
 
 	return (count);
 }
