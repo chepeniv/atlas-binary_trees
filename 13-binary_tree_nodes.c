42f30e54b668cb5c81ee16434ec53363b88e1374
chepeniv <jose.olmos@atlasschool.com>
2024-08-10 14:47:41 -0500

    [passing] created function

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - measures the height of a tree
+ * @node: point from which to count from
+ *
+ * Return: height measured
+ */
+size_t binary_tree_nodes(const binary_tree_t *node)
+{
+	size_t count;
+
+	if (node == NULL || (node->left == NULL && node->right == NULL))
+		return (0);
+
+	count = 1;
+	count += binary_tree_nodes(node->left);
+	count += binary_tree_nodes(node->right);
+
+	return (count);
+}
