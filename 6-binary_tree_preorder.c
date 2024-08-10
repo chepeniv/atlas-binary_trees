ef6e0f5454a3661b3207a7bdcc3deff27fc60203
chepeniv <jose.olmos@atlasschool.com>
2024-08-10 11:22:06 -0500

    [untested] changed algorithm to recursive

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,29 +9,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *root, void (*func)(int))
 {
-	const binary_tree_t *current;
-	binary_tree_t **stacktop;
-
 	if (root == NULL || func == NULL)
 		return;
 
-	stacktop = malloc(sizeof(void *));
-	*stacktop = stack_create();
-
-	current = root;
-	while (current != NULL)
-	{
-		func(current->n);
-		if (current->left != NULL)
-		{
-			if (current->right != NULL)
-				stack_push(stacktop, current->right);
-			current = current->left;
-		}
-		else if (current->right != NULL)
-			current = current->right;
-		else
-			current = stack_pop(stacktop);
-	}
-	free(stacktop);
+	func(root->n);
+	binary_tree_preorder(root->left, func);
+	binary_tree_preorder(root->right, func);
 }
