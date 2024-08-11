0a2e64d187ad0eae2e5e56cc79fd4f93bb8036df
chepeniv <jose.olmos@atlasschool.com>
2024-08-11 14:31:21 -0500

    [doc] completed doc

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,10 @@
 #include "binary_trees.h"
 
-int is_perfect_eng(const binary_tree_t *node);
-
 /**
- * binary_tree_is_perfect - determines whether a given tree is perfect
- * @node: point from which to analyze from
+ * binary_tree_sibling - finds and the sibling of a given node
+ * @node: point to find the sibling of
  *
- * Return: if true: 1, else: 0
+ * Return: pointer to the sibling if found
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
